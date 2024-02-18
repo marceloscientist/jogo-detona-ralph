fb9ddd8844c8798949e9e3b7b5e6a8293a20134e
Marcelo Carvalho <marcelo.santana@edu.sc.senai.br>
2024-02-18 09:00:38 -0300

    Base final

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,13 +1,30 @@
 const state = {
     view: {
-        squares: document.querySelectorAll('.square'),
-        enemy: document.querySelector('.enemy'),
-        timeLeft: document.querySelector('#time-left'),
-        score: document.querySelectorAll('#score'),
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
     },
     values: {
-        timerId: null,
         gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 10,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+}
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.timerId);
+        clearInterval(state.actions.countDownTimerId);
+        alert(`Game Over! O seu resultado foi: ${state.values.result}`);
     }
 }
 
@@ -16,21 +33,33 @@ function randomSquare() {
         square.classList.remove('enemy');
     });
 
-    let randomNumber = Math.floor(Math.random() * state.view.squares.length);
+    let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add('enemy');
+    state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy() { 
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+function playSound(audioName) {
+    let audio = new Audio(`../audio/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
 }
 
 function addListenerHitBox() {
-    state.view.squares.forEach((square) => {});
-}
+    state.view.squares.forEach((square) => {
+      square.addEventListener("mousedown", () => {
+        if (square.id === state.values.hitPosition) {
+          state.values.result++;
+          state.view.score.textContent = state.values.result;
+          state.values.hitPosition = null;
+          playSound('hit');
+        }
+      });
+    });
+  }
 
 function init() {
-    moveEnemy();
+    addListenerHitBox();
 }
 
 init();
